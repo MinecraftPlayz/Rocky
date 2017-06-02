163fdaf492294ea3c9232986a587925b1cf43c58
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-06-02 12:28:21 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "version\nmakeserver\nstop\n" | php src/pocketmine/PocketMine.php --no-wizard | grep -v "\[Tesseract] Adding "
-if ls plugins/Tesseract/Rocky*.phar >/dev/null 2>&1; then
+if ls plugins/Tesseract/Rocky_1.0dev*.phar >/dev/null 2>&1; then
     echo Server packaged successfully.
 else
     echo No phar created!
