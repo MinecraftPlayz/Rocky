c9be58d41c9ed3dc5922873753013c6675127c35
wesleyvanneck <wesleyvanneck1012@gmail.com>
2017-06-04 14:20:36 +0200

    Update run.sh

--- ci/run.sh
+++ ci/run.sh
@@ -18,7 +18,7 @@ if [ $? -ne 0 ]; then
 fi
 
 echo -e "version\nmakeserver\nstop\n" | "$PHP_BINARY" -dphar.readonly=0 src/pocketmine/PocketMine.php --no-wizard --disable-ansi --disable-readline --debug.level=2
-if ls plugins/Rocky/Rocky*.phar >/dev/null 2>&1; then
+if ls plugins/Rocky/Rocky_1.0dev.phar >/dev/null 2>&1; then
     echo Server phar created successfully.
 else
     echo No phar created!
